b326159cd6da22af646b91b2ed3177e3281f6e09
mozoroff <mozoshima@gmail.com>
2022-11-16 01:29:16 +0900

    Dealer Class をDealer Objectに変更、Dealer ObjectにinitialCardsを追加

--- src/main/kotlin/Dealer.kt
+++ src/main/kotlin/Dealer.kt
@@ -1,23 +1,27 @@
-class Dealer {
+object Dealer {
     // 参加人数を受け取り、それぞれのプレイヤーにカードを配る処理を作成します。
-    companion object {
-        fun startGame(amountOfPlayers: Int): ArrayList<ArrayList<Card>> {
+
+        fun startGame(table: Table): ArrayList<ArrayList<Card>> {
             //新しいデッキを作ります
             val deck = Deck()
             //デッキをシャッフルします
             deck.shuffleDeck()
-            val table: ArrayList<ArrayList<Card>> = arrayListOf()
+            val playerCards: ArrayList<ArrayList<Card>> = arrayListOf()
             // プレーヤの手札
-            for (i in 0..amountOfPlayers) {
+            for (i in 0..table.amountOfPlayers) {
                 // arrayListOfとArrayListのちがいを見つける
-                val playerHand: ArrayList<Card> = ArrayList(2)
-                for (j in 0 until 2) {
+                val playerHand: ArrayList<Card> = ArrayList(initialCards(table.gameMode))
+                for (j in 0 until initialCards(table.gameMode)) {
                     val card1 = deck.draw()
                     playerHand.add(card1)
                 }
-                table.add(playerHand)
+                playerCards.add(playerHand)
             }
-            return table
+            return playerCards
         }
-    }
+    fun initialCards(gameMode: String): Int {
+            if(gameMode == "poker") return 5
+            if(gameMode == "21") return 2
+            else return 0
+        }
 }
