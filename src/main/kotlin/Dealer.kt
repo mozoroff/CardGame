ee547baa775231735db9e0656c18d2dcd353694b
mozoroff <mozoshima@gmail.com>
2022-11-16 02:14:09 +0900

    Dealer ObjectにprintTableInformationメソッドを追加

--- src/main/kotlin/Dealer.kt
+++ src/main/kotlin/Dealer.kt
@@ -8,7 +8,7 @@ object Dealer {
             deck.shuffleDeck()
             val playerCards: ArrayList<ArrayList<Card>> = arrayListOf()
             // プレーヤの手札
-            for (i in 0..table.amountOfPlayers) {
+            for (i in 0 until table.amountOfPlayers) {
                 // arrayListOfとArrayListのちがいを見つける
                 val playerHand: ArrayList<Card> = ArrayList(initialCards(table.gameMode))
                 for (j in 0 until initialCards(table.gameMode)) {
@@ -24,4 +24,17 @@ object Dealer {
             if(gameMode == "21") return 2
             else return 0
         }
+    // 卓の情報を表示するメソッドを作成します。table を受け取り、卓の情報とそれぞれのプレイヤーの手札を表示します。
+    fun printTableInformation(playerCards: ArrayList<ArrayList<Card>>, table: Table){
+        println("Amount of player: ${table.amountOfPlayers}... Game mode: ${table.gameMode}. At this table: ")
+        for(i in 0 until playerCards.size){
+            println("player " + (i + 1) + " hand is:")
+            for(j in 0 until playerCards[i].size){
+                println(playerCards[i][j].getCardString())
+            }
+            println()
+        }
+    }
+
+
 }
