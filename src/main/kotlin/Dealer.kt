86e9316e7f9eb5700632655cbd7a3f82c173a05a
mozoroff <mozoshima@gmail.com>
2022-11-16 19:15:54 +0900

    HelpFunctions→HelperFunctionsiに名前変更 Dealer classにwinnerOf21メソッドを追加

--- src/main/kotlin/Dealer.kt
+++ src/main/kotlin/Dealer.kt
@@ -1,15 +1,13 @@
 object Dealer {
     // 参加人数を受け取り、それぞれのプレイヤーにカードを配る処理を作成します。
-
         fun startGame(table: Table): ArrayList<ArrayList<Card>> {
             //新しいデッキを作ります
             val deck = Deck()
             //デッキをシャッフルします
             deck.shuffleDeck()
-            val playerCards: ArrayList<ArrayList<Card>> = arrayListOf()
+            val playerCards: ArrayList<ArrayList<Card>> = ArrayList()
             // プレーヤの手札
             for (i in 0 until table.amountOfPlayers) {
-                // arrayListOfとArrayListのちがいを見つける
                 val playerHand: ArrayList<Card> = ArrayList(initialCards(table.gameMode))
                 for (j in 0 until initialCards(table.gameMode)) {
                     val card1 = deck.draw()
@@ -39,11 +37,35 @@ object Dealer {
     // ブラックジャックでは値の合計値が21を超えるとNGなのでその場合は0とします。
     fun score21Individual(cards: ArrayList<Card>): Int{
         var value: Int = 0
-        for(i in 0 until cards.size){
+        for(i in cards.indices){
             value += cards[i].intValue
         }
         if(value > 21) value = 0
         return value
+    }
+
+    // ブラックジャックで誰が勝利したか表示する関数を作成します。
+    // それぞれのプレイヤーの手札をscore21Individualで計算し、配列に保存します。例: [10,16,15,16,15,15]
+    // この場合、勝利するプレイヤーが複数存在することからHashMapでcacheを作成し、
+    // cache[10] = 1, cache[15] = 3, cache[16] = 2のように書き換えます。
+    // 配列 [10,16,15,16,15,15]の最大値は16で、cache[16] > 1なのでドローになります。
+    // もし、0 <= cache[16] <= 1なら、そのプレイヤーの勝利、それ以外の場合は勝者が誰もいないことになります。
+    // ではこのロジックを関数にしてみましょう。
+    fun winnerOf21(playerCards: ArrayList<ArrayList<Card>>): String {
+        val cache: HashMap<Int, Int?> = HashMap()
+        val points: IntArray = IntArray(playerCards.size)
+
+        for (i in playerCards.indices) {
+            val point: Int = score21Individual(playerCards[i])
+            points[i]= point
+            if (cache[point] == null) {
+                cache[point] = 1
+            } else cache.replace(point, cache[point]!! + 1)
+        }
+        val winnerIndex: Int = HelperFunctions.maxInArrayIndex(points)
+        if (cache[points[winnerIndex]]!! > 1) return "It is a draw "
+        else if (cache[points[winnerIndex]]!! >= 0) return "player ${winnerIndex + 1} is winner"
+        else return "No winners.."
     }
 
 }
--- src/main/kotlin/HelperFunctions.kt
+++ src/main/kotlin/HelperFunctions.kt
@@ -1,10 +1,9 @@
-object HelpFunctions {
+object HelperFunctions {
     // 数値で構成される配列を受け取り、最大値のインデックスを返します。
     fun maxInArrayIndex(intArr: IntArray): Int{
         var maxIndex: Int = 0
         var maxValue: Int =  intArr[0]
-        for(i in intArr.indices)
-        {
+        for(i in 1 until intArr.size){
             if(intArr[i] > maxValue) {
                 maxValue = intArr[i]
                 maxIndex = i
